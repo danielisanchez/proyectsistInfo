1fb0c96d51f950b293a5bae9b946ebe1d42ca197
SeaneCorrea <43554602+SeaneCorrea@users.noreply.github.com>
2019-04-03 02:07:47 -0400

    Ahora el host si sirve

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBafSPh7H_let-SltPQtEV-MjH2a8S3gvU",
+    authDomain: "proysistemasinfo.firebaseapp.com",
+    databaseURL: "https://proysistemasinfo.firebaseio.com",
+    projectId: "proysistemasinfo",
+    storageBucket: "proysistemasinfo.appspot.com",
+    messagingSenderId: "768619126709"
+  }
 };
